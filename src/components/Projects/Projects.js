33d5ac26e268e52a41171c38299a5f27a7631129
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-16 22:57:30 -0400

    project media queries finished

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ import Gitcards from "../../img/gitcards.png";
 class Projects extends Component {
   render() {
     return (
-      <div className='body'>
+      <div style={{ display: "flex" }}>
         <div className='container'>
           <Projectsinfo
             ProjectImg={Ecommerce}
@@ -27,80 +27,56 @@ class Projects extends Component {
           <Projectsinfo
             ProjectImg={R2Hhackathon}
             ProjectTitle='Revival Hackathon'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='hackathon project that was built in a group of a group of 6 and was made with basic html and css. Project was to help promote helping poverish areas find opprotunities'
             Github='https://github.com/codyg7/R2H-Hackathon'
             Live='https://revival-r2h-hackathon.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={Gateway}
             ProjectTitle='Gateway Project'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='Project that was built out of html and css to advertise Ireland. Purpose was for a interview process. Was able to get into the program with this product and was revised to make sure it was semantic'
             Github='https://github.com/codyg7/GatewayProject'
             Live='https://gatewayproject-ireland.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={TicTacToe}
             ProjectTitle='Tic-Tac-Toe'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='React Tic-Tac-Toe app that was built to help introduce us to react and react (find)'
             Github='https://github.com/codyg7/tic-tac-toe'
             Live='https://tic-tac-toe-reactapp.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={Navbar}
             ProjectTitle='Responsive Navigation'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='Responsive Navigation bar that was built with html and css. Was able to remake a Responsive Navigation bar using react, this portfolio is currently utilizing the react version.'
             Github='https://github.com/codyg7/Animated-Navbar'
             Live='https://responsive-navbar0.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={ToDoList}
             ProjectTitle='To Do List'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='To do list built with react to introduce us to props, components, and constructors.'
             Github='https://github.com/codyg7/To-Do-ReactList'
             Live='https://r2hreact-todo-list.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={Voice}
             ProjectTitle='Voice Api'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='Number guessing game using vanilla JS API for speech recognition to watch for when the user guesses a number and tells the user either higher or lower till they guess the correct number'
             Github='https://github.com/codyg7/SpeechRecognitionAPI'
             Live='https://guessinggame-voicerec.netlify.app/'
           />
           <Projectsinfo
             ProjectImg={Gitcards}
             ProjectTitle='Github profile cards'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='Utilizing a github API to introduce us to Axios to search GitHub accounts and show the accounts name, avatar, and company'
             Github='https://github.com/codyg7/react-github-cards'
             Live='https://github-cards0.netlify.app/'
           />
           <Projectsinfo
             ProjectImg='../../../public/img/E-commerce.png'
             ProjectTitle='Pokemon'
-            ProjectDesc='Utilizing React to build the client side, Node to send the
-          cient side information that was put into the Contact form and
-          sending it to the Database which was build with Mongo, its
-          server built with Express, and deployed onto Heroku'
+            ProjectDesc='Built with Node, and Inquirer to take in user information match it with JSON information and log it back out attatched to a string'
             Github='https://github.com/codyg7/Pokemon'
             Live=''
           />
